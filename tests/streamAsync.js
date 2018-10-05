817efe6c32bfb06d8fcc4ea03dc8b0aca5b9c01e
Gilles Piou <gilles.piou@gmail.com>
2018-10-05 17:25:25 +0800

    Setup Prettier

--- tests/streamAsync.js
+++ tests/streamAsync.js
@@ -2,166 +2,160 @@
 "use strict";
 
 var gSpawn = require("../"),
-	Stream = require("stream"),
-	Vinyl = require("vinyl"),
-	assert = require("assert"),
-	es = require("event-stream");
+  Stream = require("stream"),
+  Vinyl = require("vinyl"),
+  assert = require("assert"),
+  es = require("event-stream");
 
-describe("gulp-spawn", function () {
+describe("gulp-spawn", function() {
+  describe("in stream mode", function() {
+    it("should work with async contents streams", function(done) {
+      var stream = gSpawn({
+        cmd: "cat"
+      });
 
-	describe("in stream mode", function () {
+      var inputStream = new Stream.PassThrough({ objectMode: true }),
+        outputStream = new Stream.PassThrough({ objectMode: true }),
+        n = 0;
 
-		it("should work with async contents streams", function (done) {
+      var fakeFile = new Vinyl({
+        cwd: "./",
+        base: "test",
+        path: "test/file.js",
+        contents: new Stream.PassThrough()
+      });
+      var fakeFile2 = new Vinyl({
+        cwd: "./",
+        base: "test",
+        path: "test/file2.js",
+        contents: new Stream.PassThrough()
+      });
 
-			var stream = gSpawn({
-				cmd: "cat"
-			});
+      inputStream.pipe(stream).pipe(outputStream);
 
-			var inputStream = new Stream.PassThrough({objectMode: true}),
-				outputStream = new Stream.PassThrough({objectMode: true}),
-				n = 0;
+      outputStream.on("readable", function() {
+        var newFile;
+        while ((newFile = outputStream.read())) {
+          assert(newFile);
+          assert.equal(newFile.cwd, "./");
+          assert.equal(newFile.base, "test");
+          assert(newFile.contents instanceof Stream);
+          if (++n === 1) {
+            assert.equal(newFile.path, "test/file.js");
+            newFile.contents.pipe(
+              es.wait(function(err, data) {
+                assert.equal(data, "plipplap");
+              })
+            );
+          } else {
+            assert.equal(newFile.path, "test/file2.js");
+            newFile.contents.pipe(
+              es.wait(function(err, data) {
+                assert.equal(data, "plopplup");
+              })
+            );
+          }
+        }
+      });
 
-			var fakeFile = new Vinyl({
-				cwd: "./",
-				base: "test",
-				path: "test/file.js",
-				contents: new Stream.PassThrough()
-			});
-			var fakeFile2 = new Vinyl({
-				cwd: "./",
-				base: "test",
-				path: "test/file2.js",
-				contents: new Stream.PassThrough()
-			});
+      outputStream.on("end", function() {
+        assert.equal(n, 2);
+        done();
+      });
 
-			inputStream
-				.pipe(stream)
-				.pipe(outputStream);
+      inputStream.write(fakeFile);
+      inputStream.write(fakeFile2);
+      inputStream.end();
 
+      setImmediate(function() {
+        fakeFile.contents.write("plip");
+        setImmediate(function() {
+          fakeFile.contents.write("plap");
+          fakeFile.contents.end();
+        });
+      });
 
-			outputStream.on("readable", function () {
-				var newFile;
-				while (newFile = outputStream.read()) {
-					assert(newFile);
-					assert.equal(newFile.cwd, "./");
-					assert.equal(newFile.base, "test");
-					assert(newFile.contents instanceof Stream);
-					if (++n === 1) {
-						assert.equal(newFile.path, "test/file.js");
-						newFile.contents.pipe(es.wait(function (err, data) {
-							assert.equal(data, "plipplap");
-						}));
-					} else  {
-						assert.equal(newFile.path, "test/file2.js");
-						newFile.contents.pipe(es.wait(function (err, data) {
-							assert.equal(data, "plopplup");
-						}));
-					}
-				}
-			});
+      setImmediate(function() {
+        fakeFile2.contents.write("plop");
+        setImmediate(function() {
+          fakeFile2.contents.write("plup");
+          fakeFile2.contents.end();
+        });
+      });
+    });
 
-			outputStream.on("end", function () {
-				assert.equal(n, 2);
-				done();
-			});
+    it("should work with async files streams", function(done) {
+      var stream = gSpawn({
+        cmd: "cat"
+      });
 
-			inputStream.write(fakeFile);
-			inputStream.write(fakeFile2);
-			inputStream.end();
+      var inputStream = new Stream.PassThrough({ objectMode: true }),
+        outputStream = new Stream.PassThrough({ objectMode: true }),
+        n = 0;
 
-			setImmediate(function () {
-				fakeFile.contents.write("plip");
-				setImmediate(function () {
-					fakeFile.contents.write("plap");
-					fakeFile.contents.end();
-				});
-			});
+      var fakeFile = new Vinyl({
+        cwd: "./",
+        base: "test",
+        path: "test/file.js",
+        contents: new Stream.PassThrough()
+      });
+      var fakeFile2 = new Vinyl({
+        cwd: "./",
+        base: "test",
+        path: "test/file2.js",
+        contents: new Stream.PassThrough()
+      });
 
-			setImmediate(function () {
-				fakeFile2.contents.write("plop");
-				setImmediate(function () {
-					fakeFile2.contents.write("plup");
-					fakeFile2.contents.end();
-				});
-			});
+      inputStream.pipe(stream).pipe(outputStream);
 
-		});
+      outputStream.on("readable", function() {
+        var newFile;
+        while ((newFile = outputStream.read())) {
+          assert(newFile);
+          assert.equal(newFile.cwd, "./");
+          assert.equal(newFile.base, "test");
+          assert(newFile.contents instanceof Stream);
+          if (++n === 1) {
+            assert.equal(newFile.path, "test/file.js");
+            newFile.contents.pipe(
+              es.wait(function(err, data) {
+                assert.equal(data, "plipplap");
+              })
+            );
+          } else {
+            assert.equal(newFile.path, "test/file2.js");
+            newFile.contents.pipe(
+              es.wait(function(err, data) {
+                assert.equal(data, "plopplup");
+              })
+            );
+          }
+        }
+      });
 
-		it("should work with async files streams", function (done) {
+      outputStream.on("end", function() {
+        assert.equal(n, 2);
+        done();
+      });
 
-			var stream = gSpawn({
-				cmd: "cat"
-			});
+      setImmediate(function() {
+        inputStream.write(fakeFile);
+        fakeFile.contents.write("plip");
+        setImmediate(function() {
+          fakeFile.contents.write("plap");
+          fakeFile.contents.end();
+        });
 
-			var inputStream = new Stream.PassThrough({objectMode: true}),
-				outputStream = new Stream.PassThrough({objectMode: true}),
-				n = 0;
-
-			var fakeFile = new Vinyl({
-				cwd: "./",
-				base: "test",
-				path: "test/file.js",
-				contents: new Stream.PassThrough()
-			});
-			var fakeFile2 = new Vinyl({
-				cwd: "./",
-				base: "test",
-				path: "test/file2.js",
-				contents: new Stream.PassThrough()
-			});
-
-			inputStream
-				.pipe(stream)
-				.pipe(outputStream);
-
-
-			outputStream.on("readable", function () {
-				var newFile;
-				while (newFile = outputStream.read()) {
-					assert(newFile);
-					assert.equal(newFile.cwd, "./");
-					assert.equal(newFile.base, "test");
-					assert(newFile.contents instanceof Stream);
-					if (++n === 1) {
-						assert.equal(newFile.path, "test/file.js");
-						newFile.contents.pipe(es.wait(function (err, data) {
-							assert.equal(data, "plipplap");
-						}));
-					} else  {
-						assert.equal(newFile.path, "test/file2.js");
-						newFile.contents.pipe(es.wait(function (err, data) {
-							assert.equal(data, "plopplup");
-						}));
-					}
-				}
-			});
-
-			outputStream.on("end", function () {
-				assert.equal(n, 2);
-				done();
-			});
-
-			setImmediate(function () {
-				inputStream.write(fakeFile);
-				fakeFile.contents.write("plip");
-				setImmediate(function () {
-					fakeFile.contents.write("plap");
-					fakeFile.contents.end();
-				});
-
-				setImmediate(function () {
-					inputStream.write(fakeFile2);
-					inputStream.end();
-					fakeFile2.contents.write("plop");
-					setImmediate(function () {
-						fakeFile2.contents.write("plup");
-						fakeFile2.contents.end();
-					});
-				});
-			});
-
-		});
-
-	});
-
+        setImmediate(function() {
+          inputStream.write(fakeFile2);
+          inputStream.end();
+          fakeFile2.contents.write("plop");
+          setImmediate(function() {
+            fakeFile2.contents.write("plup");
+            fakeFile2.contents.end();
+          });
+        });
+      });
+    });
+  });
 });
